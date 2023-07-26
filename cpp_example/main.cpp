012a1b71043fb7c84602c7e5d10feb128b518174
famarker <arunav@famark.com>
2023-07-26 16:32:58 +0530

    Added logic to perform CRUD on Business_Contact entity

--- cpp_example/main.cpp
+++ cpp_example/main.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 int main()
 {
+    /*IMPORTANT:: CHECK THE README FILE BEFORE RUNNING THE CODE*/
     FamarkCloudAPI cloudAPI;
-
-    // Taking Domain name, Username, Password from user
+    //Taking DomainName, Username, Password from user
     string domainName, userName, password;
-    cout << "Enter Domain Name: \n";
+    cout << "Enter Domain Name: ";
     cin >> domainName;
-    cout << "Enter User Name: \n";
+    cout << "Enter User Name: ";
     cin >> userName;
-    cout << "Enter Password: \n";
+    cout << "Enter Password: ";
     cin >> password;
 
     Json::Value credData;
@@ -44,26 +44,30 @@ int main()
         return -1;
     }
 
-    Json::Value profileData;
+    Json::Value contactData;
     // Creating records by taking input from user
     char userResponse;
     cout << "\nDo you want to create a new record (y/n): \n";
     cin >> userResponse;
-    while (userResponse == 'Y' || userResponse == 'y')
+    while (userResponse == 'Y' || userResponse == 'y') 
     {
-        string displayName, systemName;
-        cout << "Enter Display Name: \n";
-        // Clearing new lines from input stream
-        while (getchar() != '\n');
-        getline(cin, displayName);
-        cout << "Enter System Name: \n";
-        getline(cin, systemName);
+        string firstName, lastName, phone, email;
+        cout << "Enter FirstName: ";
+        cin >> firstName;
+        cout << "Enter LastName: ";
+        cin >> lastName;
+        cout << "Enter Phone: ";
+        cin >> phone;
+        cout << "Enter Email: ";
+        cin >> email;
 
-        profileData["DisplayName"] = displayName;
-        profileData["SystemName"] = systemName;
+        contactData["FirstName"] = firstName;
+        contactData["LastName"] = lastName;
+        contactData["Phone"] = phone;
+        contactData["Email"] = email;
 
-        //Calling CreateRecord action on System_Profile entity of System solution
-        string recordData = cloudAPI.post_data("/System_Profile/CreateRecord", profileData.toStyledString(), sessionId);
+        //Calling CreateRecord action on Business_Contact entity of Business solution
+        string recordData = cloudAPI.post_data("/Business_Contact/CreateRecord", contactData.toStyledString(), sessionId);
 
         if (!reader.parse(recordData, responseJson)) {
             cout << reader.getFormattedErrorMessages();
@@ -74,8 +78,8 @@ int main()
 
         if (!recordId.empty()) {
             cout << "Created Record Id: \n" << recordId;
-            //Login to your famark cloud domain to see the newly created record under System => Profile
-            cout << "\nDo you want to create more System record (y/n)? \n";
+            //Login to your famark cloud domain to see the newly created record under Business > Contact
+            cout << "\nDo you want to create more contact record (y/n)? \n";
         }
         else {
             cout << "\nDo you want to try again (y/n)? \n";
@@ -83,11 +87,11 @@ int main()
         cin >> userResponse;
     }
 
-    //Calling RetrieveMultipleRecords action on the System_Profile entity
+    //Calling RetrieveMultipleRecords action on the Business_Contact entity
     Json::Value retrieveRequest;
-    retrieveRequest["Columns"] = "DisplayName, SystemName";
-    retrieveRequest["OrderBy"] = "DisplayName";
-    const string records = cloudAPI.post_data("/System_Profile/RetrieveMultipleRecords", retrieveRequest.toStyledString(), sessionId);
+    retrieveRequest["Columns"] = "FullName, Phone, Email, Business_ContactId";
+    retrieveRequest["OrderBy"] = "FullName";
+    const string records = cloudAPI.post_data("/Business_Contact/RetrieveMultipleRecords", retrieveRequest.toStyledString(), sessionId);
 
     Json::Value recordObjects;
 
@@ -98,14 +102,80 @@ int main()
 
     int numRecords = recordObjects.size();
     cout << "\nNumber of Records = " << numRecords;
-    cout << "\nRecords present inside entity are: \n";
+    cout << "\nRecords present in response are:\n";
 
     Json::Value recordObject;
 
     for (Json::Value::ArrayIndex i = 0; i < numRecords; i++) {
-        // Set in tmp the json_object of the recordObjects array at index i
+        //Set in tmp the json_object of the recordObjects array at index i
         recordObject = recordObjects[i];
         cout << "\n Record no.: " << i + 1 << " " << recordObject.toStyledString();
+    }
+
+    //Updating records by taking input from user
+    char userUpdateResponse;
+    cout << "\nDo you want to update a new record (y/n): \n";
+    cin >> userUpdateResponse;
+    while (userUpdateResponse == 'Y' || userUpdateResponse == 'y')
+    {
+        string firstName, lastName, phone, email, contactId;
+        cout << "Enter ContactId: ";
+        cin >> contactId;
+        cout << "Enter FirstName: ";
+        cin >> firstName;
+        cout << "Enter LastName: ";
+        cin >> lastName;
+        cout << "Enter Phone: ";
+        cin >> phone;
+        cout << "Enter Email: ";
+        cin >> email;
+
+        contactData["Business_ContactId"] = contactId;
+        contactData["FirstName"] = firstName;
+        contactData["LastName"] = lastName;
+        contactData["Phone"] = phone;
+        contactData["Email"] = email;
+
+        //Calling UpdateRecord action on Business_Contact entity of Business solution
+        string recordData = cloudAPI.post_data("/Business_Contact/UpdateRecord", contactData.toStyledString(), sessionId);
+
+        if (!reader.parse(recordData, responseJson)) {
+            cout << reader.getFormattedErrorMessages();
+            return -2;
+        }
+
+        const string recordId = responseJson.asString();
+
+        if (!recordId.empty()) {
+            cout << "Updated Record Id: " << recordId;
+            //Login to your famark cloud domain to see the newly created record under Business > Contact
+            cout << "\nDo you want to update more contact record (y/n)? \n";
+        }
+        else {
+            cout << "\nDo you want to try again (y/n)? \n";
+        }
+
+        cin >> userUpdateResponse;
+    }
+
+    //Delete records by taking input from user
+    char userDeletingResponse;
+    cout << "\nDo you want to delete a new record (y/n): \n";
+    cin >> userDeletingResponse;
+    while (userDeletingResponse == 'Y' || userDeletingResponse == 'y')
+    {
+        string contactId;
+        cout << "Enter ContactId: ";
+        cin >> contactId;
+
+        contactData["Business_ContactId"] = contactId;
+
+        //Calling DeleteRecord action on Business_Contact entity of Business solution
+        string recordData = cloudAPI.post_data("/Business_Contact/DeleteRecord", contactData.toStyledString(), sessionId);
+
+        cout << "Record Deleted";
+        cout << "\nDo you want to delete a new record (y/n): \n";
+        cin >> userDeletingResponse;
     }
 
     return 0;
